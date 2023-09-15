1cf91194a5faf5155b93a6e8aa7eea922979ba5a
Utkarshtewari07 <utkarshtewari80@gmail.com>
2023-09-16 02:28:09 +0530

    Task management app

---
+++ src/test/java/com/project/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.taskmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
